16289c0346cec10d1c6a180c0a3ce0ed1d4b64d4
andre <tdc@gmx.ch>
2016-04-09 23:42:05 +0200

    gulp cleanup

--- HomeControl/src/HomeControl.Web/gulpfile.js
+++ HomeControl/src/HomeControl.Web/gulpfile.js
@@ -1,6 +1,4 @@
-﻿/// <binding AfterBuild='copy:font' />
-/// <reference path="bower_components/startbootstrap-sb-admin-2/dist/js/sb-admin-2.js" />
-/// <binding AfterBuild='copy:css, copy:font, copy:js' />
+﻿/// <binding BeforeBuild='copy:css, copy:js, copy:font' />
 /*
 This file in the main entry point for defining Gulp tasks and using Gulp plugins.
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
