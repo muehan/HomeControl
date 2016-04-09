53ddf56703e3680c39799cb4e5156b89ab7a2fda
andre <tdc@gmx.ch>
2016-04-09 23:19:58 +0200

    added sb-admin-2 menu

--- HomeControl/src/HomeControl.Web/gulpfile.js
+++ HomeControl/src/HomeControl.Web/gulpfile.js
@@ -1,4 +1,5 @@
-﻿/// <binding AfterBuild='copy:css, copy:font, copy:js' />
+﻿/// <reference path="bower_components/startbootstrap-sb-admin-2/dist/js/sb-admin-2.js" />
+/// <binding AfterBuild='copy:css, copy:font, copy:js' />
 /*
 This file in the main entry point for defining Gulp tasks and using Gulp plugins.
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
@@ -17,10 +18,15 @@ paths.bootstrapCss = "./bower_components/bootstrap/dist/css/bootstrap.css";
 paths.sbAdminCss = "./bower_components/startbootstrap-sb-admin-2/dist/css/sb-admin-2.css";
 paths.fontAwesomeCss = "./bower_components/font-awesome/css/font-awesome.css";
 paths.morrisCss = "./bower_components/morrisjs/morris.css";
+paths.metisMenuCss = "./bower_components/metismenu/src/metisMenu.css";
+
 
 paths.jqueryJs = "./bower_components/jquery/dist/jquery.js";
 paths.raphaelJs = "./bower_components/raphael/raphael.js";
 paths.morrisJs = "./bower_components/morrisjs/morris.js";
+paths.bootstrapJs = "./bower_components/bootstrap/dist/js/bootstrap.js";
+paths.sbAdminJs = "./bower_components/startbootstrap-sb-admin-2/dist/js/sb-admin-2.js";
+paths.metisMenuJs = "./bower_components/metismenu/src/metisMenu.js";
 
 paths.fonts = "./bower_components/font-awesome/fonts/*";
 
@@ -29,26 +35,26 @@ paths.cssDest = paths.webroot + "css";
 paths.fontDest = paths.webroot + "fonts";
 
 gulp.task('min:js', function () {
-    return gulp.src([paths.jqueryJs, paths.raphaelJs, paths.morrisJs])
+    return gulp.src([paths.jqueryJs, paths.raphaelJs, paths.morrisJs, paths.bootstrapJs, paths.sbAdminJs, paths.metisMenuJs])
         .pipe(concat(paths.jsDest + "/min/site.min.js"))
         .pipe(uglify())
         .pipe(gulp.dest("."));
 });
 
 gulp.task("copy:js", function() {
-    return gulp.src([paths.jqueryJs, paths.raphaelJs, paths.morrisJs])
+    return gulp.src([paths.jqueryJs, paths.raphaelJs, paths.morrisJs, paths.bootstrapJs, paths.sbAdminJs, paths.metisMenuJs])
         .pipe(gulp.dest(paths.jsDest));
 });
 
 gulp.task('min:css', function () {
-    return gulp.src([paths.bootstrapCss, paths.sbAdminCss, paths.fontAwesomeCss, paths.morrisCss])
+    return gulp.src([paths.bootstrapCss, paths.sbAdminCss, paths.fontAwesomeCss, paths.morrisCss, paths.metisMenuCss])
         .pipe(concat(paths.cssDest + "/min/site.min.css"))
         .pipe(cssmin())
         .pipe(gulp.dest("."));
 });
 
 gulp.task("copy:css", function () {
-    return gulp.src([paths.bootstrapCss, paths.sbAdminCss, paths.fontAwesomeCss, paths.morrisCss])
+    return gulp.src([paths.bootstrapCss, paths.sbAdminCss, paths.fontAwesomeCss, paths.morrisCss, paths.metisMenuCss])
         .pipe(gulp.dest(paths.cssDest));
 });
 
