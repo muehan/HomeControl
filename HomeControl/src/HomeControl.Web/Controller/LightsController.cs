c1deab5e8dbb7f8788b691d82efa69b221be8f61
andre <tdc@gmx.ch>
2016-05-01 21:06:20 +0200

    added controllers and views, started with XML

---
+++ HomeControl/src/HomeControl.Web/Controller/LightsController.cs
@@ -0,0 +1,13 @@
+﻿namespace HomeControl.Web.Controller
+{
+    using Microsoft.AspNet.Mvc;
+
+    public class LightsController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ HomeControl/src/HomeControl.Web/Controller/TemperaturController.cs
@@ -0,0 +1,18 @@
+﻿namespace HomeControl.Web.Controller
+{
+    using Microsoft.AspNet.Mvc;
+
+    public class TemperaturController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Outdoor()
+        {
+            return View();
+        }
+
+        public IActionResult Indoor(string room)
+        {
+            return View();
+        }
+    }
+}
