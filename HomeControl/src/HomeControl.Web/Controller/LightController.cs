0d580c938dc2f7a36a11e730f6ed980743094ea2
muehan <tdc@gmx.ch>
2016-08-21 19:28:49 +0200

    added light controller

---
+++ HomeControl/src/HomeControl.Web/Controller/LightController.cs
@@ -0,0 +1,14 @@
+﻿
+namespace HomeControl.Web.Controller
+{
+    using Microsoft.AspNet.Mvc;
+
+    public class LightController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
